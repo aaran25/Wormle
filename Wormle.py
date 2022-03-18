292e956dcb70f650ae6c1d81e7eb1b05aa184635
aaran25 <99447618+aaran25@users.noreply.github.com>
2022-03-17 19:04:42 -0700

    Update Wormle.py

--- Wormle.py
+++ Wormle.py
@@ -7,7 +7,7 @@ with open("fivewords.txt",'r') as f:
 #a = f.find(',' + ',')
 #print(a)
 
-guess = random.randrange(0,5724)
+guess = random.randrange(0,5719)
 print(guess)
 
 
@@ -19,21 +19,41 @@ print(cat)
 
 First_Attempt = input("00000\n")
 #Green
-for i in range(5):
+'''for i in range(5):
     if First_Attempt[i] == cat[i]:
-        print(("\033[1;32;20m Bright Green  \n"))
+        print("\033[1;32;20m Bright Green  \n")
     #In cont. Yellow
-    elif First_Attempt[i] != cat[i]:
-        for i in range(5):
+    #elif First_Attempt[i] in cat:
+        dog = cat.find(First_Attempt[i])
+        print(dog)
+        cat = cat[0:dog] + cat[dog+1 : len(cat)]
+        print(cat)
+
+
 #gray
     else:
-        print(("\033[1;30;20m Dark Gray  \n"))
+        print("\033[1;30;20m Dark Gray  \n")'''
+
+#elif First_attempt[]
 
 
 
 
 
+Dictionary = {"e":1}
+for i in range (len(cat)):
+    if cat[i] in Dictionary.keys():
+        Dictionary[cat[i]] += 1
+    else:
+        Dictionary[cat[i]] = 1
+
+print(Dictionary)
+#ictionary[cat]
 
 
 
 #Color code
+#print("\033[1;32;40m Bright Green  \n")
+
+
+
