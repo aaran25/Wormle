e86381d154c8d9745787e834a2be908ea24220f9
aaran25 <99447618+aaran25@users.noreply.github.com>
2022-05-05 19:01:22 -0700

    working version

--- Wormle.py
+++ Wormle.py
@@ -2,75 +2,33 @@
 import random
 with open("fivewords.txt",'r') as f:
     content = f.read()
-    print(content)
-
-#a = f.find(',' + ',')
-#print(a)
 
 guess = random.randrange(0,5718)
-print(guess)
-
 
 c = content.replace('\n', ',')
 c = c.split(",")
-print(c)
+
 cat = c[guess]
 print(cat)
 
-First_Attempt = input("00000\n")
-#Green
-for i in range(5):
-    if First_Attempt[i] == cat[i]:
-        print("\033[1;32;20m Bright Green  \n")
-    #In cont. Yellow
-    #elif First_Attempt[i] in cat:
-        dog = cat.find(First_Attempt[i])
-        print(dog)
-        cat = cat[0:dog] + cat[dog+1 : len(cat)]
-        print(cat)
 
-
-#gray
-    else:
-        print("\033[1;30;20m Dark Gray  \n")
-
-#elif First_attempt[]
-
-
-
-
-
-Dictionary = {"e":1}
+Dictionary = {}
 for i in range (len(cat)):
     if cat[i] in Dictionary.keys():
         Dictionary[cat[i]] += 1
     else:
         Dictionary[cat[i]] = 1
-
-for i in range(len(cat)):
-    if cat[i] in Dictionary and Dictionary[cat[i]] >= cat.count(cat[i]):
-        print ("i made it in")
-        if First_Attempt[i] == cat[i]:
-            print("\033[1;32;20m Bright Green  \n")
-        elif:
-            cat[i] in Dictionary and Dictionary[cat[i]] >= cat.count(cat[i]):
-                print("\033[95m {}\033[00m".format(skk))
-        else:
-            print("\033[1;30;20m Dark Gray  \n")
-
-
-
-
-
-
-print(Dictionary)
-#ictionary[cat]
-
-
-
-#Color code
-#print("\033[1;32;40m Bright Green
-
-
+for i in range(5):
+    First_Attempt = input("Your guess?")
+    for i in range(1):
+        for i in range(len(cat)):
+            if First_Attempt[i] == cat[i]:
+                print("\033[1;32;20m Green  \n")
+            elif First_Attempt[i] in Dictionary.keys() and Dictionary[First_Attempt[i]] >= First_Attempt.count(First_Attempt[i]):
+                print("\033[1;33;20m Yellow\n")
+            elif guess in Dictionary:
+                cat[i] in Dictionary and Dictionary[cat[i]] >= cat.count(cat[i])
+            else:
+                print("\033[1;30;20m Gray \n")
 
 
