8c5887d99a8bf7cddf91be8057d910e5cecc93f5
aaran25 <99447618+aaran25@users.noreply.github.com>
2022-05-19 18:36:01 -0700

    wokring

--- Wormle.py
+++ Wormle.py
@@ -2,15 +2,13 @@
 import random
 with open("fivewords.txt",'r') as f:
     content = f.read()
-
+tries = int("0")
 guess = random.randrange(0,5718)
 
 c = content.replace('\n', ',')
 c = c.split(",")
 
 cat = c[guess]
-print(cat)
-
 
 Dictionary = {}
 for i in range (len(cat)):
@@ -18,17 +16,18 @@ for i in range (len(cat)):
         Dictionary[cat[i]] += 1
     else:
         Dictionary[cat[i]] = 1
-for i in range(5):
+while  tries < 5:
     First_Attempt = input("Your guess?")
-    for i in range(1):
+    if First_Attempt in (c):
+        tries + 1
         for i in range(len(cat)):
             if First_Attempt[i] == cat[i]:
-                print("\033[1;32;20m Green  \n")
+                print("\033[1;32;20m Green\n" + First_Attempt[i])
             elif First_Attempt[i] in Dictionary.keys() and Dictionary[First_Attempt[i]] >= First_Attempt.count(First_Attempt[i]):
-                print("\033[1;33;20m Yellow\n")
+                print("\033[1;33;20m\n" + First_Attempt[i])
             elif guess in Dictionary:
                 cat[i] in Dictionary and Dictionary[cat[i]] >= cat.count(cat[i])
             else:
-                print("\033[1;30;20m Gray \n")
-
-
+                print("\033[1;30;20m\n" + First_Attempt[i])
+    elif First_Attempt not in (c):
+        print ("Not quite stupid. I hate to say this, but try again")
